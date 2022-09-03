9f42c60a4c6710875c6ef792c6f0ffeec3de12da
lnaidu14 <lalit.pratap.naidu@gmail.com>
2022-09-03 00:43:00 -0400

    Merge dev into prod (#2)
    Feature: Components and Dynamic Endpoint
    * Installed Bootstrap
    
    * Navbar, Cards and dynamic API endpoint (#1)
    
    * Added Card, Jumbotron and basic routing
    * Added cat data
    * Created API endpoint for dynamically displaying cat data

---
+++ components/Jumbotron/Jumbotron.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Jumbotron() {
+  return (
+    <>
+      <div class="jumbotron jumbotron-fluid mt-5">
+        <div class="container">
+          <h1 class="display-4">Cats Adoption</h1>
+          <p class="lead">Find your purrfect match and a mew best friend!</p>
+          <button type="button" class="btn btn-primary">
+            Start Looking
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Nav() {
+  return (
+    <>
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar
+          </a>
+        </div>
+      </nav>
+    </>
+  );
+}
